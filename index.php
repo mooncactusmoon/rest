07bb2b479014f0c2fb92b4d4ba27f98f7ef05720
Moon <mooncactusmoon@gmail.com>
2022-02-16 16:19:15 +0800

    加上switch判斷

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
+echo $_SERVER['REQUEST_URI']; #發請求的是誰
+echo "<hr>";
+echo $_SERVER['REQUEST_METHOD']; #使用甚麼方法
+echo "<hr>";
+echo "HIII";
+switch($_SERVER['REQUEST_METHOD']){
+   case "GET":
+      echo "取得資料";
+   break;
+   case "POST":
+      echo "新增資料";
+   break;
+   case "PUT":
+      echo "更新資料";
+   break;
+   case "DELETE":
+      echo "刪除資料";
+   break;
+}
 
-dd($_SERVER);
+
+// dd($_SERVER);
 
 function dd($array){
    echo "<pre>";
